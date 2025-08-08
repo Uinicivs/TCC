f88039e6422284d31e415378031a3202435d30b8
Siqueira <siqueiraguilherme920@gmail.com>
2025-08-08 16:06:24 -0300

    feat: add DottedBackground component with radial gradient styling

---
+++ client/src/components/shared/DottedBackground.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full h-full dot-background">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.dot-background {
+  background-image: radial-gradient(circle, var(--color-background-mute) 1px, transparent 1px);
+  background-size: 1.5em 1.5em;
+}
+</style>
