b50d9ab9329392c3e769a348b783873c14e49c29
Siqueira <siqueiraguilherme920@gmail.com>
2025-10-28 19:34:13 -0300

    feat: create warning tooltip component

---
+++ client/src/components/shared/WarningTooltip.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{ message: string }>()
+</script>
+
+<template>
+  <div
+    class="absolute -right-6 -top-2 z-10 flex items-center justify-center cursor-default"
+    v-tooltip="{
+      value: message,
+      pt: {
+        root: { style: { maxWidth: '350px' } },
+        arrow: { style: { borderRightColor: 'var(--p-amber-500)' } },
+        text: '!bg-amber-500 !font-medium',
+      },
+    }"
+  >
+    <i class="pi pi-exclamation-triangle text-amber-500" />
+  </div>
+</template>
